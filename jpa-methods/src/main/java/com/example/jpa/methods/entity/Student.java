463b962fc43e2a3d618ae8985fa70e15d3f1a03d
anusha-ivo <anusha525@gmail.com>
2026-01-29 23:17:29 +0530

    methods

---
+++ jpa-methods/src/main/java/com/example/jpa/methods/entity/Student.java
@@ -0,0 +1,36 @@
+package com.example.jpa.methods.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+@Entity
+public class Student {
+    @Id
+    private Integer id;
+    private String name;
+    private Integer age;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+}
